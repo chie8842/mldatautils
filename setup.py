3c403e7dcab4216bcf8c30f13171b3256b949bbb
chie8842 <chie-hayashida@cookpad.com>
2019-09-12 16:08:32 +0900

    release 0.1.2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='mldatautils',
-    version='0.1.1',
+    version='0.1.2',
     description="mldatautils",
     long_description="mldatautils",
     download_url='https://pypi.org/project/mldatautils',
