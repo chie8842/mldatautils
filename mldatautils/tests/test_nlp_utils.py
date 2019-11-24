a44b142e76ffee07539efc72bfb32b18f1af4a57
chie8842 <chie-hayashida@cookpad.com>
2019-11-24 22:45:58 +0900

    update test

--- mldatautils/tests/test_nlp_utils.py
+++ mldatautils/tests/test_nlp_utils.py
@@ -1,5 +1,5 @@
-from datautils.nlp_utils import TokenFilter
-from datautils.nlp_utils import Vectorizer
+from mldatautils.nlp_utils import TokenFilter
+from mldatautils.nlp_utils import Vectorizer
 from gensim.models.fasttext import FastText
 from gensim.models.word2vec import Word2Vec
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -22,61 +22,122 @@ def test_use_all():
     actual = TokenFilter.use_all(parsed_sentence1)
     assert expected == actual
 
+    # test use_jaconv
+    expected = [
+        '鍋', 'に', '湯', 'を', '沸かす', '、', 'タピオカ', 'が',
+        '透明', 'に', 'なる', 'まで', '茹でる', '、', '(', '1', '5', '分',
+        'から', '1', '7', '分', '位', ')', 'ザル', 'に', 'あげる', 'て',
+        '水', 'で', '洗う', 'ます', '。'
+    ]
+    actual = TokenFilter.use_all(parsed_sentence1, use_jaconv=True)
+    assert expected == actual
 
-def test_use_noun():
-    expected = ['鍋', '湯', 'タピオカ', '透明', '１', '５', '分', '１', '７', '分', '位', 'ザル', '水']
-    actual = TokenFilter.use_noun(parsed_sentence1)
+    # test pos_list
+    expected = [
+        '鍋', '湯', '沸かす', 'タピオカ',
+        '透明', 'なる', '茹でる', '１', '５', '分',
+        '１', '７', '分', '位', 'ザル', 'あげる',
+        '水', '洗う',
+    ]
+    actual = TokenFilter.use_all(parsed_sentence1, pos_list=['名詞', '動詞'])
+    assert expected == actual
+
+    # except_words
+    expected = [
+        'に', '湯', 'を', '沸かす', '、', 'が',
+        '透明', 'に', 'なる', 'まで', '茹でる', '、', '（', '１', '５', '分',
+        'から', '１', '７', '分', '位', '）', 'ザル', 'に', 'あげる', 'て',
+        '水', 'で', '洗う', 'ます', '。'
+    ]
+    actual = TokenFilter.use_all(parsed_sentence1, except_words=['鍋', 'タピオカ'])
+    assert expected == actual
+
+    # test exclude_numbers
+    expected = [
+        '鍋', 'に', '湯', 'を', '沸かす', '、', 'タピオカ', 'が',
+        '透明', 'に', 'なる', 'まで', '茹でる', '、', '（', '分',
+        'から', '分', '位', '）', 'ザル', 'に', 'あげる', 'て',
+        '水', 'で', '洗う', 'ます', '。'
+    ]
+    actual = TokenFilter.use_all(parsed_sentence1, exclude_numbers=True)
+    assert expected == actual
+
+    # test exclude_symbols
+    expected = [
+        '鍋', 'に', '湯', 'を', '沸かす', 'タピオカ', 'が',
+        '透明', 'に', 'なる', 'まで', '茹でる', '１', '５', '分',
+        'から', '１', '７', '分', '位', 'ザル', 'に', 'あげる', 'て',
+        '水', 'で', '洗う', 'ます'
+    ]
+    actual = TokenFilter.use_all(parsed_sentence1, exclude_symbols=True)
+    assert expected == actual
+
+    # test exclude_symbols
+    expected = '会議するぞぉ'
+    text = '会議するぞぉ~♡ ＼(^^)／'
+    actual = TokenFilter.remove_special_characters(text)
     assert expected == actual
 
 
 def test_vectorizer_fasttext():
     vectorizer = Vectorizer('fasttext', 'models/vectorizer/fasttext/fasttext_ipadic_8.model')
+    word_list = TokenFilter.use_all(parsed_sentence1)
+    words_list = [TokenFilter.use_all(parsed_sentence1), TokenFilter.use_all(parsed_sentence2)]
+
     assert type(vectorizer.model) == FastText
     assert vectorizer.model_dim == 8
 
-    actual = vectorizer.transform_sentence(parsed_sentence1, TokenFilter.use_all, True)
+    actual = vectorizer.transform_sentence(word_list)
     assert actual.shape == (8,)
 
-    actual = vectorizer.transform_sentences(parsed_sentences, TokenFilter.use_all, True, 8)
+    actual = vectorizer.transform_sentences(words_list, 8)
     assert actual.shape == (2, 8)
 
 
 def test_vectorizer_word2vec():
     vectorizer = Vectorizer('word2vec', 'models/vectorizer/word2vec/word2vec_ipadic_8.model')
+    word_list = TokenFilter.use_all(parsed_sentence1)
+    words_list = [TokenFilter.use_all(parsed_sentence1), TokenFilter.use_all(parsed_sentence2)]
+
     assert type(vectorizer.model) == Word2Vec
     assert vectorizer.model_dim == 8
 
-    actual = vectorizer.transform_sentence(parsed_sentence1, TokenFilter.use_all, True)
+    actual = vectorizer.transform_sentence(word_list)
     assert actual.shape == (8,)
 
-    actual = vectorizer.transform_sentences(parsed_sentences, TokenFilter.use_all, True, 8)
+    actual = vectorizer.transform_sentences(words_list, 8)
     assert actual.shape == (2, 8)
 
 
 def test_vectorizer_tfidf():
     vectorizer = Vectorizer('tfidf', 'models/vectorizer/tfidf/tfidf_ipadic_30000.model')
+    word_list = TokenFilter.use_all(parsed_sentence1)
+    words_list = [TokenFilter.use_all(parsed_sentence1), TokenFilter.use_all(parsed_sentence2)]
+
     assert type(vectorizer.model) == TfidfVectorizer
     assert vectorizer.model_dim <= 30000
 
-    actual = vectorizer.transform_sentence(parsed_sentence1, TokenFilter.use_all, True)
-    assert actual.shape == (vectorizer.model_dim, )
+    actual = vectorizer.transform_sentence(word_list)
+    assert actual.shape == (vectorizer.model_dim,)
 
-    actual = vectorizer.transform_sentences(parsed_sentences, TokenFilter.use_all, True, vectorizer.model_dim)
-    assert actual.shape == (2, vectorizer.model_dim)
+    actual = vectorizer.transform_sentences(words_list, 8)
+    assert actual.shape == (2, 2)
 
 
 def test_decompose():
-    parsed_sentences_large = parsed_sentences
+    words_list = [TokenFilter.use_all(parsed_sentence1), TokenFilter.use_all(parsed_sentence2)]
     for i in range(10):
-        parsed_sentences_large = parsed_sentences_large + parsed_sentences_large
-    assert len(parsed_sentences_large) == 2048
+        words_list = words_list + words_list
+    assert len(words_list) == 2048
 
     vectorizer = Vectorizer('tfidf', 'models/vectorizer/tfidf/tfidf_ipadic_30000.model')
-    actual = vectorizer.transform_sentences(parsed_sentences_large, TokenFilter.use_all, True, 100)
+    actual = vectorizer.transform_sentences(words_list, 100)
     assert actual.shape == (2048, 100)
 
 
 def test_fail_transform_sentences():
     vectorizer = Vectorizer('fasttext', 'models/vectorizer/fasttext/fasttext_ipadic_8.model')
-    with pytest.raises(Exception) as e_info:
-       actual = vectorizer.transform_sentences(parsed_sentences_large, TokenFilter.use_all, True, 16)
+    words_list = [TokenFilter.use_all(parsed_sentence1), TokenFilter.use_all(parsed_sentence2)]
+
+    actual = vectorizer.transform_sentences(words_list, 16)
+    assert actual is None
